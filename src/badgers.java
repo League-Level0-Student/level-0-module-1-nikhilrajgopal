f59a425270865336737a8b6575f3fe4cf2bd4be0
nikhilrajgopal <league@WTS-IM-17.attlocal.net>
2018-03-22 19:58:54 -0700

    Badger, my ages

---
+++ src/badgers.java
@@ -0,0 +1,12 @@
+
+public class badgers {
+public static void main(String[] args) {
+	for(int i=0; i<10; i++) {
+		System.out.println("Badger");
+	}
+	for(int i=0; i<2; i++) {
+		System.out.println("Mushroom");
+	}
+	for(int i=0; i)
+}
+}
