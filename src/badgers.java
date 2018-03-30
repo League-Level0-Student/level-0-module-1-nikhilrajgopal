933052954e346b67a4df962ebd6c54b18096b7ec
nikhilrajgopal <league@wts-im-17.attlocal.net>
2018-03-29 19:23:50 -0700

    Badger, badger, badger

--- src/badgers.java
+++ src/badgers.java
@@ -1,12 +1,17 @@
 
 public class badgers {
 public static void main(String[] args) {
+	for(int J=0; J<3; J++) {
 	for(int i=0; i<10; i++) {
 		System.out.println("Badger");
 	}
 	for(int i=0; i<2; i++) {
 		System.out.println("Mushroom");
 	}
-	for(int i=0; i)
+	}
+	for(int i=0; i<2; i++) {
+		System.out.println("A snake");
+	}
 }
 }
+
